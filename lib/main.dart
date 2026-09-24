f1cfb8ac72494fb672de0c962c28bebc5c56e832
shivashisvicky <shivashisvicky112@gmail.com>
2026-09-24 10:18:16 +0530

    Initialize Rive native runtime

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:rive/rive.dart' as rive;
 import 'app.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await rive.RiveNative.init();
   runApp(const SuperBookApp());
 }
